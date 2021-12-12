c9d58fc51459561e58d09f022bc9e1b99293409f
PranaliShingare <91608042+PranaliShingare@users.noreply.github.com>
2021-12-12 23:59:18 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+var input = document.querySelector('.input_text');
+var main = document.querySelector('#name');
+var temp = document.querySelector('.temp');
+var desc = document.querySelector('.desc');
+var clouds = document.querySelector('.clouds');
+var searchbutton= document.querySelector('.search').value;
+
+
+searchbutton.addEventListener('click', function(name){
+fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid=50a7aa80fa492fa92e874d23ad061374'+'&units=metric')
+.then(response => response.json())
+.then(data => {
+  var tempValue = data['main']['temp'];
+  var nameValue = data['name'];
+  var descValue = data['weather'][0]['description'];
+
+  main.innerHTML = nameValue;
+  desc.innerHTML = descValue;
+  temp.innerHTML = tempValue;
+  input.value ="";
+
+})
+
+
+})
